1fdddc992b3e1c6cadcbbdbc6a166ad9bc3e1c5e
julioignaciootero <julioignaciootero@gmail.com>
2023-02-03 13:51:59 -0300

    Readme twlio

--- src/controllers/whatsapp.js
+++ src/controllers/whatsapp.js
@@ -1,17 +1,19 @@
 import dotenv from 'dotenv';
 import { twilioClient } from "../services/wspservices.js";
+import { logger } from "../config/logs.js";
 dotenv.config();
 
 
 export const sendWS = async(user, carrito)  => {
     try {
       const message = {
-        body: "carrito comprado",
+        body: `Orden de compra: ${carrito._id}`,
         from: process.env.CEL,
-        to: 'whatsapp+5491121714727',
+        to: 'whatsapp:+5491121714727',
       };
       const response = await twilioClient.messages.create(message);
-      res.json(response);
+      logger.info("Wsp enviado")
+    //   res.json(response);
     } catch (error) {
       console.log(error);
     }
